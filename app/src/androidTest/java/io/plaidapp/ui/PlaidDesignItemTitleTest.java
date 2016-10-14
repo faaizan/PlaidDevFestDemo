d692ededf5c79b09bcbac014414709aadc5934ab
shivrajp130@gmail.com <Appleios55>
2016-10-15 00:34:08 +0530

    - Modified tests

---
+++ app/src/androidTest/java/io/plaidapp/ui/PlaidDesignItemTitleTest.java
@@ -0,0 +1,78 @@
+package io.plaidapp.ui;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.plaidapp.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class PlaidDesignItemTitleTest {
+
+    @Rule
+    public ActivityTestRule<HomeActivity> mActivityTestRule = new ActivityTestRule<>(HomeActivity.class);
+
+    @Test
+    public void designItemTitleTest() {
+
+        onView(withId(R.id.grid)).perform(scrollToPosition(0));
+
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.menu_filter), withContentDescription("Filter"), isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        pressBack();
+
+        onView(withId(R.id.grid))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(3, click()));
+
+        pressBack();
+
+        onView(withId(R.id.grid)).perform(scrollToPosition(4));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
--- app/src/androidTest/java/io/plaidapp/ui/ViewPagerTest.java
+++ app/src/androidTest/java/io/plaidapp/ui/ViewPagerTest.java
@@ -33,7 +33,7 @@ import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class PagerAssertion {
+public class ViewPagerTest {
 
     @Rule
     public ActivityTestRule<HomeActivity> mActivityTestRule = new ActivityTestRule<>(HomeActivity.class);
